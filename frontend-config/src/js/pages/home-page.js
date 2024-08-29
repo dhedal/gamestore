170cd9c593d40509345b001a15ebb09941153ad8
dhedal <hedgardavid@yahoo.fr>
2024-08-29 11:44:13 +0200

    ajout du fichier game-service.js pour les requetes api, implementation de la responsivité du menu de navigation

--- frontend-config/src/js/pages/home-page.js
+++ frontend-config/src/js/pages/home-page.js
@@ -1,6 +1,11 @@
 import { PageComponent } from "components/page-component.js";
+import { GameService} from "../services/game-service.js";
 
 export class HomePage extends PageComponent {
+    dataLastGameMap;
+    dataCurrentPromotionMap;
+    lastGamesContainer;
+    currentPromotionsContainer;
 
     constructor() {
         super("/home", "Accueil", "/html/home.html", ["js/views/home.js"]);
@@ -8,10 +13,29 @@ export class HomePage extends PageComponent {
 
     async before() {
         console.log("## homePage # before");
+        this.dataLastGameMap = new Map();
+        this.dataCurrentPromotionMap = new Map();
+
+        await Promise.all([
+            GameService.fetchHomePageData().then(response => {
+                response.lastGames.forEach(game => {
+                    this.dataLastGameMap.set(game.uuid, game);
+                });
+                response.currentPromotions.forEach(game => {
+                    this.dataCurrentPromotionMap.set(game.uuid, game);
+                });
+            })
+        ]);
+
+        this.lastGamesContainer = document.getElementById("lastGamesContainer");
+        this.currentPromotionsContainer = document.getElementById("currentPromotionsContainer");
     }
 
     ready() {
         console.log("## HomePage # ready");
+        console.log(this.dataLastGameMap);
+        console.log("-----------");
+        this.console.log(this.dataCurrentPromotionMap);
 
     }
 
